e484b35b88c69baf285f9b2102ec6441f410da13
markliou <yifanliou@markliou.tw>
2024-01-14 14:56:52 +0800

    fix token-vocabulary number issue

--- python/llama.cpp_tools/down_hf_model_snapshot.py
+++ python/llama.cpp_tools/down_hf_model_snapshot.py
@@ -1,4 +1,5 @@
 from huggingface_hub import snapshot_download
 model_id="lmsys/vicuna-13b-v1.5"
-snapshot_download(repo_id=model_id, local_dir="vicuna-hf",
+download_loc = "vicuna-hf"
+snapshot_download(repo_id=model_id, local_dir=download_loc,
                   local_dir_use_symlinks=False, revision="main")
